474a100afbcb18e47a18b05feec82ec2195747e8
Nathan Foster <nathancfoster.03@gmail.com>
2021-08-30 11:13:58 -0700

    w3d1 innit

---
+++ algos/w3d1.js
@@ -0,0 +1,64 @@
+/* case insensitive string compare */
+
+const strA1 = "ABC";
+const strB1 = "abc";
+const expected1 = true;
+
+const strA2 = "ABC";
+const strB2 = "abd";
+const expected2 = false;
+
+const strA3 = "ABC";
+const strB3 = "bc";
+const expected3 = false;
+
+function checkString(str1, str2) {
+    var str1Lower = str1.toLowerCase();
+    var str2Lower = str2.toLowerCase();
+    if (str1Lower == str2Lower) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(checkString(strA1, strB1));
+
+/**
+ * Determines whether or not the strings are equal, ignoring case.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} strA
+ * @param {string} strB
+ * @returns {boolean} If the strings are equal or not.
+ */
+
+
+function caseInsensitiveStringCompare(strA, strB) {
+
+}
+
+
+// /* 
+//   Acronyms
+//   Create a function that, given a string, returns the string’s acronym 
+//   (first letter of each word capitalized). 
+//   Do it with .split first if you need to, then try to do it without
+// */
+
+const str1 = " there's no free lunch - gotta pay yer way. ";
+const expected1 = "TNFL-GPYW";
+
+const str2 = "Live from New York, it's Saturday Night!";
+const expected2 = "LFNYISN";
+
+/**
+ * Turns the given str into an acronym.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str A string to be turned into an acronym.
+ * @returns {string} The given str converted into an acronym.
+ */
+function acronymize(str) {
+
+}
